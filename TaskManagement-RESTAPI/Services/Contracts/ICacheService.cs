c01256e22a6d865a660cc92914011ad63b35e856
PrashanthSai-K <srisathyasai24680@gmail.com>
2025-04-12 15:00:00 +0530

    Implemented Redis Caching system for efficient utilization of resource and faster response time

--- TaskManagement-RESTAPI/Services/Contracts/ICacheService.cs
+++ TaskManagement-RESTAPI/Services/Contracts/ICacheService.cs
@@ -4,7 +4,7 @@ namespace TaskManagement_RESTAPI.Services.Contracts;
 
 public interface ICacheService
 {
-    Task<T?> GetCacheAsync<T>(string key);
+    Task<T?> GetCacheAsync<T>(string key) ;
     Task SetCacheAsync<T>(string key, T value, TimeSpan? expiry = null);
     Task RemoveCacheAsync<T>(string key);
     Task AddKeyToSetAsync(string key, string cacheKey);
